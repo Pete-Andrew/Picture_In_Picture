9de8c5288667166e4b25426f70523e83bc11340f
Peter Andrew <peter.a.andrew@gmail.com>
2024-03-24 20:31:15 +0000

    test

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 //Prompts the user to select a media stream, pass to video element, then play
-
+// await keyword needs async function to work (as of 2024). 
 async function selectMediaStream() {
    try {
     // const that holds the media stream data which awaits until the user picks which screen is needed
+    //The mediaDevices read-only property of the Navigator interface returns a MediaDevices object, which provides access to connected media input devices like cameras and microphones, as well as screen sharing.
+
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     // passed in the the media stream into the video element
     videoElement.srcObject = mediaStream;
@@ -13,7 +15,6 @@ async function selectMediaStream() {
     videoElement.onloadedmetadata = () => {
         videoElement.play();
     }
-
 }
     catch (error) {
     // catches any errors
@@ -26,11 +27,10 @@ button.addEventListener('click', async () => {
     button.disabled = true;
     // Start picture in picture
     await videoElement.requestPictureInPicture();
-
+    // if the video appears the button is then re-enabled. 
     button.disabled = false;
 });
 
 //on Load
-
 selectMediaStream();
 
